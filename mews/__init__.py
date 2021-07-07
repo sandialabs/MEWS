ce8863bcdbde354166163c16c3f1a4a6301962ee
Daniel Villa <36861196+dlvilla@users.noreply.github.com>
2021-07-07 15:01:29 -0500

    fixing missing files

---
+++ mews/__init__.py
@@ -0,0 +1,14 @@
+from mews import weather
+from mews import stats
+from mews import cython
+from mews import epw
+
+
+__version__ = '0.0.0'
+
+__copyright__ = """Copyright 2019 National Technology & Engineering 
+Solutions of Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 
+with NTESS, the U.S. Government retains certain rights in this software."""
+
+
+__license__ = "Revised BSD License"
