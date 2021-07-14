f5d3f1040118587d39f1a1f38e384090d895dea9
Daniel Villa <36861196+dlvilla@users.noreply.github.com>
2021-07-14 10:02:31 -0500

    Update __init__.py

--- mews/cython/__init__.py
+++ mews/cython/__init__.py
@@ -3,4 +3,4 @@ The Building Energy Modeling Extreme Weather Simulator (BEMEWS)
 weather module reads in, alters, and writes out weather files
 to account for extreme events or climate change.
 """
-from mews.cython.markov import markov_chain
+from mews.cython.markov cimport markov_chain
