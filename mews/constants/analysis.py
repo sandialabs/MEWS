b2369ffe70f8cf342249e08c7437de2058b185a9
Daniel Villa <dlvilla@ecw00604.sandia.gov>
2022-10-30 15:22:04 -0600

    adding analysis.py

---
+++ mews/constants/analysis.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 27 15:40:43 2022
+
+@author: dlvilla
+"""
+
+DEFAULT_RANDOM_SEED = 2398294
+
+DEFAULT_SOLVER_NUMBER_STEPS = 1000000
