00a57c0da9f7c9aef88ead0b35bfb577262f5fb4
Macmillan <mmacmill@nrel.gov>
2022-11-10 10:25:16 -0700

    denver data is now included

--- examples/worcester_example.py
+++ examples/worcester_example.py
@@ -13,7 +13,7 @@ from mews.events import ExtremeTemperatureWaves
 
 
 if __name__ == "__main__":
-    step = 1
+    step = 2
     # STEP 1 to using MEWS, create a climate increase in surface temperature
     #        set of scenarios,
     # 
